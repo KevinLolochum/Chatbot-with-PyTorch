c64241bb9e74fcfcecca133b12d66e11e3d6646e
KevinLolochum <kevinlempiris@gmail.com>
2021-05-23 01:19:50 -0400

    edited train files, added some dependencies

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import json
+import nltk
 
 import torch
 import torch.nn as nn
@@ -8,6 +9,8 @@ from torch.utils.data import Dataset, DataLoader
 
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
+
+nltk.download('punkt')
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
